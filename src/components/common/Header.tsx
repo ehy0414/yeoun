0ef08b1526c291e6bb47d0fc67657311336a3e99
ehy0414 <djagusdyd78@gmail.com>
2025-10-12 14:59:02 +0900

    feat: Header 공통 컴포넌트화

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
+export default function Header() {
+    const navigate = useNavigate();
+    const onBack = () => {
+        navigate(-1);
+    }
+    return (
+        <button
+            onClick={onBack}
+            className="cursor-pointer text-gray-600 hover:text-pink-600 transition-colors"
+          >
+            ← 돌아가기
+          </button>
+    )
+}
