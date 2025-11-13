9b95d9c03fe69d197655b9cb484d79091eb3035a
ehy0414 <djagusdyd78@gmail.com>
2025-11-13 12:42:35 +0900

    feat: 페이지 router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
+        <Route path='/home' element={<HomePage />}/>
+        <Route path='/signUp' element={<SignUpPage />} />
+        <Route path='/' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
